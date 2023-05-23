6f05c50abac3b6ed25faa349a8ebd754c9fc14e7
EtienneMar <etienne.martinc@gmail.com>
2023-05-24 01:00:54 +0200

    Revert "Séparation de tout tentative drag and drop"
    This reverts commit 8ef24337015a3b9c781242b6e4fa36d53086fdf5.

--- frontend/src/components/Example/index.tsx
+++ frontend/src/components/Example/index.tsx
@@ -1,7 +1,5 @@
 export { default as UploadedFiles } from './UploadedFiles';
+export { default as Table } from './Table';
 export { default as Dropdown } from './Dropdown';
-export { default as FieldFileAndMappingLeft } from './FieldFileAndMappingLeft'
+export { default as FileColumns } from './FileColumns'
 export { default as DropdownFileType } from './DropdownfileType';
-export { default as FieldFile } from './FieldFile'
-export { default as MappingDroppable } from './MappingDroppable'
-export { default as MappingOutput } from './MappingOutput'
