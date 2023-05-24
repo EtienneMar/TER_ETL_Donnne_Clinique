73d83a4aecdcaca6a8db8fa5b68bbfe002218f96
EtienneMar <etienne.martinc@gmail.com>
2023-05-25 01:11:34 +0200

    Perfect DragAndDrop

--- frontend/src/components/Example/FieldFileAndMappingLeft.tsx
+++ frontend/src/components/Example/FieldFileAndMappingLeft.tsx
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import { BsLayoutThreeColumns } from 'react-icons/bs';
 import { DndProvider, useDrag, useDrop } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { Dispatch, SetStateAction } from 'react';
-
-
 
 const DraggableHeader: React.FC<{ header: string | null, setHeader?: (header: string | null) => void }> = ({ header, setHeader }) => {
   const [{ isDragging }, drag] = useDrag(() => ({
@@ -12,10 +10,8 @@ const DraggableHeader: React.FC<{ header: string | null, setHeader?: (header: st
     item: { header },
     end: (item, monitor) => {
       const dropResult = monitor.getDropResult();
-      console.log("dropResult : ", dropResult)
-      console.log("Item : ", item)
       if (item && dropResult && setHeader) {
-        setHeader("Faite Glisser le Champ correspondant"); // Reset the header value once it is dropped.
+        setHeader("Faite Glisser le Champ correspondant");
       }
     },
     collect: (monitor) => ({
@@ -23,8 +19,6 @@ const DraggableHeader: React.FC<{ header: string | null, setHeader?: (header: st
     }),
     canDrag: () => header !== "Faite Glisser le Champ correspondant"
   }));
-
-  
 
   return (
     <div ref={drag} style={{ opacity: isDragging ? 0.5 : 1 }}>
@@ -40,14 +34,12 @@ interface DroppableFieldProps {
   setDroppedItem: (item: string | null) => void;
 }
 
-
-const DroppableField: React.FC<DroppableFieldProps>  = ({ index, onDrop, droppedItem, setDroppedItem }) => {
+const DroppableField: React.FC<DroppableFieldProps> = ({ index, onDrop, droppedItem, setDroppedItem }) => {
   interface Item {header: string;}
-
   const [{ isOver }, drop] = useDrop(() => ({
     accept: 'header',
-    drop: (item : Item) => {
-      onDrop(index, item.header); // Passe l'index à la fonction onDrop
+    drop: (item: Item) => {
+      onDrop(index, item.header);
       setDroppedItem(item.header);
     },
     collect: (monitor) => ({
@@ -58,12 +50,10 @@ const DroppableField: React.FC<DroppableFieldProps>  = ({ index, onDrop, dropped
   return (
     <td
       ref={drop}
-      style={{
-        backgroundColor: isOver ? 'lightgreen' : 'white',
-      }}
+      style={{ backgroundColor: isOver ? 'lightgreen' : 'white' }}
     >
       {droppedItem && (
-          <DraggableHeader header={droppedItem} setHeader={setDroppedItem} key={droppedItem}  />
+        <DraggableHeader header={droppedItem} setHeader={setDroppedItem} key={droppedItem}  />
       )}
     </td>
   );
@@ -76,117 +66,114 @@ interface FieldFileAndMappingLeftProps {
   setunmappedHeader: Dispatch<SetStateAction<string[]>>;
 }
 
-const FieldFileAndMappingLeft: React.FC<FieldFileAndMappingLeftProps> = ({
-  uploadedFileName,
-  unmappedHeaders,
-  FieldMappingLeft,
-  setunmappedHeader, 
-}) => {
-
-  const [, setDroppedHeaders] = useState<(string)[]>([]); // Ajouter un état pour les headers déposés
-  const [initialUnmappedHeaders, setInitialUnmappedHeaders] = useState<string[]>([]);
-
-  useEffect(() => {
-    setInitialUnmappedHeaders(unmappedHeaders); // store the initial unmappedHeaders value
-  }, []);
-
-  const handleDrop = (index: number, header: string) => {
-    setunmappedHeader(prevHeaders => prevHeaders.filter(h => h !== header));
-    setDroppedHeaders(prev => {
-        const newDropped = [...prev];
-        if (unmappedHeaders.filter(h => h !==prev[index]) && prev[index] !== undefined){
-            setunmappedHeader(prevUnmapped => [...prevUnmapped, prev[index]]);
-        }
-        newDropped[index] = header; // Mettre à jour le header à l'index spécifié
-        return newDropped;
-    });
-};
+const FieldFileAndMappingLeft: React.FC<FieldFileAndMappingLeftProps> = ({uploadedFileName,unmappedHeaders,FieldMappingLeft, setunmappedHeader,}) => {
   
-  const [droppedItems, setDroppedItems] = useState<(string | null)[]>(new Array(FieldMappingLeft.length).fill("Faite Glisser le Champ correspondant"));
+  const [, setDroppedHeaders] = useState<string[]>([]);
+  const initialUnmappedHeadersRef = useRef(unmappedHeaders);
+  
+  const handleDrop = (index: number, header: string) => {
+    setunmappedHeader((prevHeaders) => prevHeaders.filter((h) => h !== header));
+    setDroppedHeaders((prev) => {
+      const newDropped = [...prev];
+      if (
+        unmappedHeaders.filter((h) => h !== prev[index]) &&
+        prev[index] !== undefined
+      ) {
+        setunmappedHeader((prevUnmapped) => [...prevUnmapped, prev[index]]);
+      }
+      newDropped[index] = header;
+      return newDropped;
+    });
+  };
 
+  const [droppedItems, setDroppedItems] = useState<(string | null)[]>(
+    new Array(FieldMappingLeft.length).fill("Faite Glisser le Champ correspondant")
+  );
+  
   const handleReset = () => {
-    setunmappedHeader(initialUnmappedHeaders); // reset unmappedHeaders to its initial value
+    setunmappedHeader(initialUnmappedHeadersRef.current);
     setDroppedItems(new Array(FieldMappingLeft.length).fill("Faite Glisser le Champ correspondant"));
   };
 
-    return (
-      <DndProvider backend={HTML5Backend}>
-        <div className="row">
-          <div className="col-lg-4 mt-3 pt-3">
-            <div className="list-group mb-3" id="file-column">
-              <li className="list-group-item d-flex align-items-center justify-content-center gap-2 text-white bg-gradient-pink">
-                <BsLayoutThreeColumns size="20" />
-                <span className="fw-semibold">
-                  {uploadedFileName
-                    ? `${uploadedFileName} - File Columns`
-                    : 'File Columns'}
-                </span>
-              </li>
-            {unmappedHeaders
-              .map((header) => (
-                <DraggableHeader header={header} key={header} />
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <div className="row">
+        <div className="col-lg-4 mt-3 pt-3">
+          <div className="list-group mb-3" id="file-column">
+            <li className="list-group-item d-flex align-items-center justify-content-center gap-2 text-white bg-gradient-pink">
+              <BsLayoutThreeColumns size="20" />
+              <span className="fw-semibold">
+                {uploadedFileName
+                  ? `${uploadedFileName} - File Columns`
+                  : "File Columns"}
+              </span>
+            </li>
+            {unmappedHeaders.map((header) => (
+              <DraggableHeader header={header} key={header} />
             ))}
-      </div>
-            <button className="btn btn-danger" onClick={handleReset}>
-              Clear File Columns
-            </button>
           </div>
-          <div className="col-lg-8 d-flex">
-            <div className="col-sm-6 mb-3">
-              <div className="table-responsive">
-                {/* First Table */}
-                <table className="table table-bordered caption-top">
-                  <caption className="pt-0 fw-semibold">Table de Mapping - Excel Input</caption>
-                  <thead className="table-dark">
-                    <tr>
-                      <th scope="col">Champ du fichier Excel Input</th>
+          <button className="btn btn-danger" onClick={handleReset}>
+            Clear File Columns
+          </button>
+        </div>
+        <div className="col-lg-8 d-flex">
+          <div className="col-sm-6 mb-3">
+            <div className="table-responsive">
+              <table className="table table-bordered caption-top">
+                <caption className="pt-0 fw-semibold">
+                  Table de Mapping - Excel Input
+                </caption>
+                <thead className="table-dark">
+                  <tr>
+                    <th scope="col">Champ du fichier Excel Input</th>
+                  </tr>
+                </thead>
+                <tbody id="tableau-element-input">
+                  {FieldMappingLeft.map((_, index) => (
+                    <tr className="border border-secondary">
+                      <DroppableField
+                        index={index}
+                        onDrop={handleDrop}
+                        droppedItem={droppedItems[index]}
+                        setDroppedItem={(item) => {
+                          setDroppedItems((items) => {
+                            const newItems = [...items];
+                            newItems[index] = item;
+                            return newItems;
+                          });
+                        }}
+                      />
                     </tr>
-                  </thead>
-                  <tbody id="tableau-element-input">
-                    {FieldMappingLeft.map((_,index) => (
-  <tr className="border border-secondary">
-  <DroppableField 
-    index={index} 
-    onDrop={handleDrop} 
-    droppedItem={droppedItems[index]} 
-    setDroppedItem={(item) => {
-      setDroppedItems(items => {
-        const newItems = [...items];
-        newItems[index] = item;
-        return newItems;
-      });
-    }} 
-  />
-</tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
+                  ))}
+                </tbody>
+              </table>
             </div>
-            <div className="col-sm-6 mb-3">
-              <div className="table-responsive">
-                {/* Second Table */}
-                <table className="table table-bordered caption-top">
-                <caption className="pt-0 fw-semibold">Table de Mapping - Mapper pour l'Output</caption>
-                  <thead className="table-dark">
-                    <tr>
-                      <th scope="col">Champ Possible à Mapper pour l'Output</th>
+          </div>
+          <div className="col-sm-6 mb-3">
+            <div className="table-responsive">
+              <table className="table table-bordered caption-top">
+                <caption className="pt-0 fw-semibold">
+                  Table de Mapping - Mapper pour l'Output
+                </caption>
+                <thead className="table-dark">
+                  <tr>
+                    <th scope="col">Champ Possible à Mapper pour l'Output</th>
+                  </tr>
+                </thead>
+                <tbody id="tableau-element-output">
+                  {FieldMappingLeft.map((value, index) => (
+                    <tr key={index} className="border border-secondary">
+                      <td>{value}</td>
                     </tr>
-                  </thead>
-                  <tbody id="tableau-element-output">
-                    {FieldMappingLeft.map((value, index) => (
-                      <tr key={index}className="border border-secondary">
-                        <td>{value}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
+                  ))}
+                </tbody>
+              </table>
             </div>
           </div>
         </div>
-      </DndProvider>
-    );      
+      </div>
+    </DndProvider>
+  );
 };
 
 export default FieldFileAndMappingLeft;
